dd9de3f59d9d5280ba59a872403a405b16b61bbc
RajHarsora <harsora@rowan.edu>
2026-01-11 16:10:15 -0500

    update figure handling; bump version to 0.3.0

--- src/project_insight_part_3/methods/homepage_figures.py
+++ src/project_insight_part_3/methods/homepage_figures.py
@@ -35,55 +35,54 @@ def pie_chart_progress() -> go.Figure:
         dynamodb = Session.resource('dynamodb')
         table = dynamodb.Table(table_name)
         response = table.scan()
+        
+        # Load data into Polars DataFrame + Clean
+        data = response['Items']
+        df = pl.DataFrame(data)
+        df = df.with_columns([
+            pl.col('start_date').str.strptime(pl.Date, format="%Y-%m-%d").alias('start_date'),
+            pl.col('end_date').str.strptime(pl.Date, format="%Y-%m-%d").alias('end_date')
+        ])
+
+        # Calculate counts
+        not_started_count = df.filter(pl.col('start_date') > dt.date.today()).height
+        in_progress_count = df.filter((pl.col('start_date') <= dt.date.today()) & (pl.col('end_date') >= dt.date.today())).height
+        completed_count = df.filter(pl.col('end_date') < dt.date.today()).height
+        participants_left = 65 - (not_started_count + in_progress_count + completed_count)
+
+        # Create pie chart
+        labels = ['Pending Start', 'In Progress', 'Completed', 'Participants Left to Recruit']
+        values = [not_started_count, in_progress_count, completed_count, participants_left]
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+        fig.update_layout(
+            title=dict(
+                text= 'Recruitment Progress',
+                x=0.5,
+                xanchor='center',
+                yanchor='top',
+                y=0.95
+            ),
+            font_size=10,
+            height=400,
+            width=350,
+            legend=dict(
+                orientation='h',
+                yanchor='bottom',
+                y=-0.5,           # move legend below chart (adjust as needed)
+                x=0.5,
+                xanchor='center',
+                font=dict(size=10)
+        ),
+            template='plotly_dark'
+        )
+
+        fig.update_layout(margin=dict(t=60, b=80))
+        # adjust overall pie size
+        fig.update_traces(marker=dict(line=dict(color='#000000', width=1)))
+        return fig
     except Exception as e:
         ui.label(f'AWS Error: {str(e)}').style('color: red; font-weight: bold;')
         return go.Figure()
-    
-    # Load data into Polars DataFrame + Clean
-    data = response['Items']
-    df = pl.DataFrame(data)
-    df = df.with_columns([
-        pl.col('start_date').str.strptime(pl.Date, format="%Y-%m-%d").alias('start_date'),
-        pl.col('end_date').str.strptime(pl.Date, format="%Y-%m-%d").alias('end_date')
-    ])
-    
-    # Calculate counts
-    not_started_count = df.filter(pl.col('start_date') > dt.date.today()).height
-    in_progress_count = df.filter((pl.col('start_date') <= dt.date.today()) & (pl.col('end_date') >= dt.date.today())).height
-    completed_count = df.filter(pl.col('end_date') < dt.date.today()).height
-    participants_left = 65 - (not_started_count + in_progress_count + completed_count)
-    
-    # Create pie chart
-    labels = ['Pending Start', 'In Progress', 'Completed', 'Participants Left to Recruit']
-    values = [not_started_count, in_progress_count, completed_count, participants_left]
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
-    fig.update_layout(
-        title=dict(
-            text= 'Recruitment Progress',
-            x=0.5,
-            xanchor='center',
-            yanchor='top',
-            y=0.95
-        ),
-        font_size=10,
-        height=400,
-        width=350,
-        legend=dict(
-            orientation='h',
-            yanchor='bottom',
-            y=-0.5,           # move legend below chart (adjust as needed)
-            x=0.5,
-            xanchor='center',
-            font=dict(size=10)
-    ),
-        template='plotly_dark'
-    )
-
-    fig.update_layout(margin=dict(t=60, b=80))
-    # adjust overall pie size
-    fig.update_traces(marker=dict(line=dict(color='#000000', width=1)))
-    
-    return fig
 
 # Fig 2: Phase breakdown Pie Chart
 def phase_breakdown_pie_chart() -> go.Figure:
